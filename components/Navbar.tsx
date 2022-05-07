fd71bd3a256e87c9bb723a02d7cfb76cd5259b81
hazar-nas <hazarcan.nas@gmail.com>
2022-05-07 15:43:06 +0300

    responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,18 +11,18 @@ import SubNavItem from './SubNavItem'
 function Navbar() {
   return (
     <div>
-      <div className="grid grid-cols-[200px_540px_300px]  justify-center  bg-primaryBlack py-4 lg:gap-x-20">
+      <div className=" flex  flex-col bg-primaryBlack lg:grid  lg:grid-cols-[200px_540px_300px]  lg:justify-center lg:gap-x-20 lg:py-4">
         {/* Logo */}
-        <div className=" relative self-center ">
+        <div className=" mb-4 lg:relative lg:mb-0 lg:self-center ">
           <h2 className=" leading-1 pb-1 font-gothic text-3xl font-bold  text-primaryGreen">
             materyum
-            <span className=" absolute top-5 right-8 font-gothic text-[16px] font-medium  tracking-[-1.2px] text-primaryGreen">
+            <span className=" absolute top-5 left-24 font-gothic  text-[16px] font-medium tracking-[-1.2px] text-primaryGreen  lg:absolute lg:right-8">
               evin için...
             </span>
           </h2>
         </div>
         {/* SearchBar */}
-        <div className="flex self-end">
+        <div className=" flex lg:self-end">
           <div className="w-full">
             <div className=" relative flex w-full flex-wrap  self-center rounded">
               <input
@@ -37,17 +37,17 @@ function Navbar() {
           </div>
         </div>
         {/* Buttons */}
-        <div className="flex self-end ">
-          <button className=" mr-4 w-1/2 rounded-2xl border bg-primaryGreen  p-1 text-sm  font-bold   text-primaryBlack transition-transform hover:scale-105 hover:border-transparent">
+        <div className=" my-2 min-w-full max-w-sm text-center lg:flex lg:self-end ">
+          <button className=" mr-5 w-1/3 rounded-md border bg-primaryGreen p-1 text-sm font-bold  text-primaryBlack transition-transform  hover:scale-105   hover:border-transparent lg:mr-4 lg:w-1/2 lg:rounded-2xl">
             Hizmet Ver
           </button>
-          <button className="mr-4 w-1/2 rounded-2xl border bg-transparent  p-1  text-sm font-semibold  text-white transition-transform hover:scale-105 ">
+          <button className=" w-1/3 truncate border bg-transparent p-1 text-sm font-semibold  text-white  transition-transform hover:scale-105  lg:mr-4 lg:w-1/2 lg:rounded-2xl ">
             Giriş Yap/Kaydol
           </button>
         </div>
       </div>
       {/* Sub NavIcon */}
-      <div className=" grid grid-cols-[900px] justify-center  bg-primaryGreen py-4">
+      <div className=" hidden bg-primaryGreen py-4 lg:grid  lg:grid-cols-[900px] lg:justify-center">
         <div className=" flex  justify-between py-1.5">
           <SubNavIcon Icon={FaWpexplorer} title="Keşfet" />
           <SubNavIcon Icon={FaBookOpen} title="Marka Kataloğu" />
@@ -55,7 +55,7 @@ function Navbar() {
         </div>
       </div>
       {/* Sub Navbar */}
-      <div className="grid grid-cols-[850px] justify-center   py-4">
+      <div className="hidden lg:grid lg:grid-cols-[850px] lg:justify-center lg:py-4">
         <div className="flex  justify-between py-1.5 font-avenir text-[14px] font-medium text-primaryBlack">
           <SubNavItem />
         </div>
